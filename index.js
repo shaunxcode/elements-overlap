619b72d45096bbb48e2df5b16dc92b57d63cc0b9
Shaun Gilchrist <shaunxcode@gmail.com>
2012-11-09 23:46:03 -0700

    note to indicate where checkOverlaps is derived

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ function getDims(elem) {
 	];
 }
 
+//Derived from https://github.com/brandonaaron/jquery-overlaps
 function checkOverlap(dims1, dims2) {
 	var x1 = dims1[1], y1 = dims1[0],
-			w1 = dims1[2], h1 = dims1[3],
-			x2 = dims2[1], y2 = dims2[0],
-			w2 = dims2[2], h2 = dims2[3];
+		w1 = dims1[2], h1 = dims1[3],
+		x2 = dims2[1], y2 = dims2[0],
+		w2 = dims2[2], h2 = dims2[3];
 	return !(y2 + h2 < y1 || y1 + h1 < y2 || x2 + w2 < x1 || x1 + w1 < x2);
 }
 
